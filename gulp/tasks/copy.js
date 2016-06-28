5c1688415bd1945596be012c47f29137d69f275f
Christian Orendi <chris.orendi@gmail.com>
2016-06-28 16:38:52 +0200

    Run the copy task for production builds, too

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,12 +2,9 @@ var $             = require('gulp-load-plugins')();
 var browserSync   = require('browser-sync');
 var config        = require('../util/loadConfig').copy;
 var gulp          = require('gulp');
-var isProduction  = require('../util/isProduction');
 
 gulp.task('copy', function() {
   browserSync.notify(config.notification);
   return gulp.src(config.assets)
-    // Write to build dir only for development builds
-    // For production builds the revision task writes the assets into the build dir
-    .pipe($.if(!isProduction, gulp.dest(config.dist)));
+    .pipe(gulp.dest(config.dist));
 });
